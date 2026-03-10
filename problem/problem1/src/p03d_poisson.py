839470d046c2a6f09ff62ca643452ba612b7219e
junliang123 <3373783227@qq.comm>
2026-03-10 19:13:45 +0800

    完成作业3

--- problem/problem1/src/p03d_poisson.py
+++ problem/problem1/src/p03d_poisson.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import util
 
@@ -14,11 +15,29 @@ def main(lr, train_path, eval_path, pred_path):
         pred_path: Path to save predictions.
     """
     # Load training set
-    x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+
     # The line below is the original one from Stanford. It does not include the intercept, but this should be added.
     # x_train, y_train = util.load_dataset(train_path, add_intercept=False)
 
     # *** START CODE HERE ***
+    x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    clf = PoissonRegression(step_size=lr)
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_eval)   
+    plt.figure()
+    plt.plot(y_eval, y_pred, 'bx')
+    plt.xlabel('true counts')
+    plt.ylabel('predict counts')
+    plt.savefig('output/p03d.png')
+
+    np.savetxt(pred_path, y_pred)
+
+    mask = (y_pred >= 0.99 * y_eval) & (y_pred <= 1.01 * y_eval)
+    N = np.sum(mask)
+    M = len(y_eval)
+    print('正确率为%d/%d' % (N, M))
+
     # Fit a Poisson Regression model
     # Run on the validation set, and use np.savetxt to save outputs to pred_path
     # *** END CODE HERE ***
@@ -41,6 +60,14 @@ class PoissonRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+        x = np.array(x)
+        m, n = x.shape
+        y = np.array(y).reshape(m, 1)
+        self.theta = np.zeros((n, 1))
+        while True:
+            last_theta = self.theta
+            self.theta = self.theta + self.step_size*x.T@(y - np.exp(x@self.theta))/m
+            if np.sum(np.abs(self.theta - last_theta)) < self.eps: break
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -53,4 +80,7 @@ class PoissonRegression(LinearModel):
             Floating-point prediction for each input, shape (m,).
         """
         # *** START CODE HERE ***
+        x = np.array(x)
+        y = np.exp(x@self.theta)
+        return y.flatten()
         # *** END CODE HERE ***
